944d3d7bbc531ec716405186e5fba20dad4afbeb
Rico <waspa@gmx.net>
2023-09-16 12:18:51 +0200

    12:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import { LandingPage } from './LandingPage/LandingPage'
+import LandingPage from './LandingPage/LandingPage'
 
 function App() {
   
 
   return (
     <>
-      <div>
-        <LandingPage/>
+      <div> <LandingPage />
+       
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
